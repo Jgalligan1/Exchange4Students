837d07944463645dcb9ee58a123a002f77867e26
Charles Villa <charlesbenjaminvilla@gmail.com>
2025-04-06 21:12:18 -0400

    Added new css and js files if needed. Implemented back tracking through the system and profile button for search screen

--- SearchBrowser.js
+++ SearchBrowser.js
@@ -105,3 +105,16 @@ function prevPage() {
     displayItems();
   }
 }
+
+function toggleDropdown() {
+  const menu = document.getElementById("dropdownMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+window.addEventListener("click", function (e) {
+  const menu = document.getElementById("dropdownMenu");
+  const profileImage = document.querySelector(".profile-image");
+  if (!profileImage.contains(e.target) && !menu.contains(e.target)) {
+    menu.style.display = "none";
+  }
+});
